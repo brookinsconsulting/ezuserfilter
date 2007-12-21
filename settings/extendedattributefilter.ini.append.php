4df2401ccca4ccbf3a278ed4723ef2588adcc2a2
Kristof Coomans <kristof.coomans@telenet.be>
2007-12-21 09:27:42 +0000

    - Initial version

---
+++ settings/extendedattributefilter.ini.append.php
@@ -0,0 +1,15 @@
+<?php /*
+
+[EnabledUserFilter]
+ExtensionName=ezuserfilter
+ClassName=eZUserExtendedAttributeFilter
+MethodName=createEnabledSqlParts
+FileName=classes/ezuserextendedattributefilter.php
+
+[DisabledUserFilter]
+ExtensionName=ezuserfilter
+ClassName=eZUserExtendedAttributeFilter
+MethodName=createDisabledSqlParts
+FileName=classes/ezuserextendedattributefilter.php
+
+*/ ?>
